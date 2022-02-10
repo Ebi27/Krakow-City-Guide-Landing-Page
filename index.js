8188b7acd691b3d2583927d1aba75c81b91a6713
Ebi27 <kpemiogokimiebi@gmail.com>
2022-02-10 19:35:42 +0100

    ➕ Added a new section for the winter information.

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const menuBtn=document.querySelector('.menu-btn');
-const navlinks= document.querySelector('.nav-links')
+//***I tried making a fading carousel but it didn't work out as expected. I'm keeping the code because i intend trying again ***//
 
-menuBtn.addEventListener('click',()=>{
-    navlinks.classList.toggle('mobile-menu')
-})
+/*const menuBtn = document.querySelector(".menu-btn");
+const navlinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navlinks.classList.toggle("mobile-menu");
+});
+
+const images = [
+  "winter1.jpeg",
+  "Winter2.jpeg",
+  "Winter3.jpeg",
+  "Winter4.jpeg",
+  "Winter5.jpeg",
+  "Winter6.jpg",
+  "Winter9.jpg",
+  "Winter8.jpeg",
+  "Winter10.jpeg",
+  "streetwinter.jpeg",
+  "winterhorse.jpeg"
+];
+const carousel =document.querySelector(".carousel");
+const interval= setInterval(function() {
+  startCarousel();
+}, 3000);
+let index=1;
+
+startCarousel = () => {
+  carousel.getElementsByClassName.backgroundImage =`url(${images[index++]})`;
+  carousel.classList.remove("fade");
+  carousel.classList.add("fade");
+  if(index > images.length - 1) index=0;
+}*/
